c7500f3afb1b2659f53e2d657038b4cf587aaa53
Juan Pablo Montalbano <juanpablomontalbano@hotmail.com.ar>
2025-01-20 20:44:36 -0300

    a

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let amigos = [];
+
+function agregarAmigo() {
+    // Obtener el valor del campo de texto
+    const nombre = document.getElementById("amigo").value.trim();
+
+    // Verificar que el nombre no esté vacío
+    if (nombre) {
+        // Añadir el nombre a la lista de amigos
+        amigosSecretos.push(nombre);
+        console.log(`${nombre} ha sido añadido a la lista de amigos secretos.`);
+        console.log("Lista actual de amigos:", amigosSecretos);
+    } else {
+        console.log("Por favor, ingresa un nombre válido.");
+    }
+}
